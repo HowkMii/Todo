a42d2bd6ce387732fe3aa2be5a5c8a0e2914b9e1
HowkMii <abdelhakim@univ-tiaret.dz>
2020-10-05 07:19:13 -0400

    add todolist

---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { TodoListContext } from "../contexts/TodoListContext";
+import Todo from "./Todo";
+
+const TodoList = () => {
+  const { todos } = useContext(TodoListContext);
+
+  return (
+    <div>
+      {todos.length ? (
+        <ul className="list">
+          {todos.map(todo => {
+            return <Todo todo={todo} key={todo.id} />;
+          })}
+        </ul>
+      ) : (
+        <div className="no-todos">No Todos</div>
+      )}
+    </div>
+  );
+};
+
+export default TodoList;
